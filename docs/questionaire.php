5810c8c49f38e114f6d3a063696e53ebb2b8afa0
BlackJesuss <31959694+BlackJesuss@users.noreply.github.com>
2017-09-14 08:56:09 -0400

    Add files via upload

---
+++ docs/questionaire.php
@@ -0,0 +1,18 @@
+ 
+<html>
+	<head>
+		<link href="indexFolder/Immigrant Calculator Css.css" rel="stylesheet" type="text/css" />
+		<link rel="shortcut icon" href="indexFolder/images/favicon.ico">
+		<link rel="icon" href="indexFolder/images/favicon.ico" type="image/ico">
+	</head>
+	
+	<title>Immigrant Calculator: Online Tool</title>
+	
+	<body>
+	
+	<?php 
+	
+	echo "hi";
+	?>
+	
+</html>
