47373a39cb81bb8481a25aa6518f089017968de6
BlackJesuss <31959694+BlackJesuss@users.noreply.github.com>
2018-03-13 23:24:19 -0400

    Add files via upload

--- index.php
+++ index.php
@@ -7,11 +7,14 @@
 		
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<meta name="theme-color" content="#BBD8B0"/>
+		
+		<script src="indexFolder/questScr.js"></script>
 		
 								<title> Immigrant Calculator </title>
 								
 		<style type="text/css">
-			.tg  {border-collapse:collapse;border-spacing:0;}
+			.tg  {border-collapse:collapse;border-spacing:0;width:90%;}
 			.tg td{font-family:Arial, sans-serif;font-size:14px;padding:10px 5px;border-style:solid;border-width:1px;overflow:hidden;word-break:normal;}
 			.tg th{font-family:Arial, sans-serif;font-size:14px;font-weight:normal;padding:10px 5px;border-style:solid;border-width:1px;overflow:hidden;word-break:normal;}
 			.tg .tg-88fa{font-weight:bold;background-color:#19a123;color:#000000;vertical-align:top}
@@ -26,46 +29,40 @@
 <?php
 $db = new mysqli('localhost','root','warbird33')
  or die('Error connecting to MySQL server.');
-
- function Check() {
-	 $confirm = $_REQUEST['date'];
-	 if ($confirm === "Lawful Permanent Resident") {
-		 $c1 = $confirm;
-	 }
-	 else {
-		  echo "CHECK IS UNSUCCESSFUL";
-	 }
- }
  
-$sql = "SELECT data FROM immgdata";
-$result = $db->query($sql);
+$sql = "SELECT id, form, Filling_Fee FROM test";
+$temp = "CREATE TEMPORARY TABLE tempimmg LIKE test";
 
-if ($result->num_rows > 0) {
-    // output data of each row
-    while($row = $result->fetch_assoc()) {
-        echo "<br> id: ". $row["id"]. " - Name: ". $row["firstname"]. " " . $row["lastname"] . "<br>";
+
+
+
+$query = "SELECT * FROM forms.test";
+
+if ($stmt = $db->prepare($query)) {
+    $stmt->execute();
+    $stmt->bind_result($field1, $field2, $field3, $field4, $field5);
+	echo "<div class='tg-wrap'><table id='tg-EhH7C' class='tg'>";
+	echo "<table id='tg-EhH7C' class='tg'>\n";
+	echo"<tr>";
+    echo"<th class='tg-88fa'>FORM</th>";
+    echo"<th class='tg-88fa'>FILLING FEE</th>";
+    echo"<th class='tg-88fa'>BIOMETRICS FEE</th>";
+    echo"<th class='tg-88fa'>TOTAL FEES</th>";
+	echo"</tr>";
+    while ($stmt->fetch()) {
+		 echo "<tr>\n"
+		  ."<td>{$field2}</td>\n"
+          ."<td>\${$field3}</td>\n"
+		  ."<td>\${$field4}</td>\n"
+		  ."<td>\${$field5}</td>\n"
+		  ."</tr>\n";
+        //printf("%s, %s, %s, %s<br/>", $field2, $field3, $field4, $field5);
     }
-} else {
-    echo "0 results";
+	echo '</table></div>'; 
+    $stmt->close();
 }
-
-$conn->close();
+	mysqli_close($db);
 ?>
-
-<div class="tg-wrap"><table id="tg-EhH7C" class="tg">
-  <tr>
-    <th class="tg-88fa">FORM</th>
-    <th class="tg-88fa">FILLING FEE</th>
-    <th class="tg-88fa">BIOMETRICS FEE</th>
-    <th class="tg-88fa">TOTAL FEES</th>
-  </tr>
-  <tr>
-    <td class="tg-yw4l" name="form"></td>
-    <td class="tg-yw4l" name="ffee"></td>
-    <td class="tg-yw4l" name="bfee"></td>
-    <td class="tg-yw4l" name="tfee"></td>
-  </tr>
-</table></div>
 <script type="text/javascript" charset="utf-8">var TgTableSort=window.TgTableSort||function(n,t){"use strict";function r(n,t){for(var e=[],o=n.childNodes,i=0;i<o.length;++i){var u=o[i];if("."==t.substring(0,1)){var a=t.substring(1);f(u,a)&&e.push(u)}else u.nodeName.toLowerCase()==t&&e.push(u);var c=r(u,t);e=e.concat(c)}return e}function e(n,t){var e=[],o=r(n,"tr");return o.forEach(function(n){var o=r(n,"td");t>=0&&t<o.length&&e.push(o[t])}),e}function o(n){return n.textContent||n.innerText||""}function i(n){return n.innerHTML||""}function u(n,t){var r=e(n,t);return r.map(o)}function a(n,t){var r=e(n,t);return r.map(i)}function c(n){var t=n.className||"";return t.match(/\S+/g)||[]}function f(n,t){return-1!=c(n).indexOf(t)}function s(n,t){f(n,t)||(n.className+=" "+t)}function d(n,t){if(f(n,t)){var r=c(n),e=r.indexOf(t);r.splice(e,1),n.className=r.join(" ")}}function v(n){d(n,L),d(n,E)}function l(n,t,e){r(n,"."+E).map(v),r(n,"."+L).map(v),e==T?s(t,E):s(t,L)}function g(n){return function(t,r){var e=n*t.str.localeCompare(r.str);return 0==e&&(e=t.index-r.index),e}}function h(n){return function(t,r){var e=+t.str,o=+r.str;return e==o?t.index-r.index:n*(e-o)}}function m(n,t,r){var e=u(n,t),o=e.map(function(n,t){return{str:n,index:t}}),i=e&&-1==e.map(isNaN).indexOf(!0),a=i?h(r):g(r);return o.sort(a),o.map(function(n){return n.index})}function p(n,t,r,o){for(var i=f(o,E)?N:T,u=m(n,r,i),c=0;t>c;++c){var s=e(n,c),d=a(n,c);s.forEach(function(n,t){n.innerHTML=d[u[t]]})}l(n,o,i)}function x(n,t){var r=t.length;t.forEach(function(t,e){t.addEventListener("click",function(){p(n,r,e,t)}),s(t,"tg-sort-header")})}var T=1,N=-1,E="tg-sort-asc",L="tg-sort-desc";return function(t){var e=n.getElementById(t),o=r(e,"tr"),i=o.length>0?r(o[0],"td"):[];0==i.length&&(i=r(o[0],"th"));for(var u=1;u<o.length;++u){var a=r(o[u],"td");if(a.length!=i.length)return}x(e,i)}}(document);document.addEventListener("DOMContentLoaded",function(n){TgTableSort("tg-EhH7C")});</script>
 
 
